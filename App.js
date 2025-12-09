86529f81ab2bed09df5718df736ceb7183ddc035
Giordano Bruno Biasi Berwig <giordano.berwig@sou.unijui.edu.br>
2025-12-09 14:23:34 -0300

    fix: updated App.js

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import HomeScreen from './src/screens/HomeScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { HomeScreen, CalendarScreen } from './src/screens';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <HomeScreen />
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Calendar" component={CalendarScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 }
