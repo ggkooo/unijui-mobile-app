f6eece628530d9e55b8c66c6a12a86899155df27
Giordano Bruno Biasi Berwig <giordano.berwig@sou.unijui.edu.br>
2026-01-20 09:46:17 -0300

    fix: updated screens/index.js

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,3 +1,5 @@
 export { default as HomeScreen } from './HomeScreen';
 export { default as CalendarScreen } from './CalendarScreen';
 export { default as SettingsScreen } from './SettingsScreen';
+export { default as AcademicScreen } from './AcademicScreen';
+export { default as CourseProgressScreen } from './CourseProgressScreen';
